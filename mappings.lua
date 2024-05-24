25ce3dc4f9611212282d0814d0fd66bc2fd9f31e
Ondrej Brablc <ondrej@brablc.com>
2024-05-24 13:50:43 +0200

    Add <leader>pi to copy clipboard to local machine using it2copy from iTerm2

--- mappings.lua
+++ mappings.lua
@@ -18,4 +18,28 @@ M.dap_python = {
   }
 }
 
+M.clipboard_to_iterm = {
+  v = {
+    ["<leader>pi"] = {
+      function()
+        vim.cmd('normal! "iy')
+        local selected_text = vim.fn.getreg('i')
+        if not selected_text or selected_text == "" then
+          print("No text selected or text is empty.")
+          return
+        end
+        local command = "/bin/bash -c '~/.iterm2/it2copy'"
+        local handle = io.popen(command, 'w')
+        if not handle then
+          print("Failed to execute command: " .. command)
+          return
+        end
+        handle:write(selected_text)
+        handle:close()
+      end,
+      desc = "Send selection to it2copy"
+    }
+  }
+}
+
 return M
